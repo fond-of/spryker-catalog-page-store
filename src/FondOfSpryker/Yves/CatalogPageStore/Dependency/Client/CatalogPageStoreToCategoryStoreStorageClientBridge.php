ada83329f921e36b7ca586f6a5b0b35f51e28234
Julian Zimmermann <julian@jzit.de>
2022-07-13 15:11:05 +0200

    prepare spryker upgrade add ci

--- src/FondOfSpryker/Yves/CatalogPageStore/Dependency/Client/CatalogPageStoreToCategoryStoreStorageClientBridge.php
+++ src/FondOfSpryker/Yves/CatalogPageStore/Dependency/Client/CatalogPageStoreToCategoryStoreStorageClientBridge.php
@@ -2,18 +2,20 @@
 
 namespace FondOfSpryker\Yves\CatalogPageStore\Dependency\Client;
 
+use Generated\Shared\Transfer\CategoryNodeStorageTransfer;
 use SprykerShop\Yves\CatalogPage\Dependency\Client\CatalogPageToCategoryStorageClientBridge as SprykerShopCatalogPageToCategoryStorageClientBridge;
 
 class CatalogPageStoreToCategoryStoreStorageClientBridge extends SprykerShopCatalogPageToCategoryStorageClientBridge implements CatalogPageStoreToCategoryStoreStorageClientInterface
 {
     /**
-     * @param int $idCategoryStorageNode
+     * @param int $idCategoryNode
      * @param string $localeName
+     * @param string $storeName
      *
      * @return \Generated\Shared\Transfer\CategoryNodeStorageTransfer
      */
-    public function getCategoryNodeById($idCategoryStorageNode, $localeName)
+    public function getCategoryNodeById(int $idCategoryNode, string $localeName, string $storeName): CategoryNodeStorageTransfer
     {
-        return $this->categoryClient->getCategoryNodeById($idCategoryStorageNode, $localeName);
+        return $this->categoryStorageClient->getCategoryNodeById($idCategoryNode, $localeName, $storeName);
     }
 }
